59e517b96ce25849f09c67e389a975bbcc64d851
kuryuo <128883817+kuryuo@users.noreply.github.com>
2026-08-04 23:59:27 +0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+      <div className="flex min-h-screen items-center justify-center bg-slate-100">
+        <h1 className="text-4xl font-bold text-blue-600">
+          LexFlow
+        </h1>
+      </div>
+  );
+}
+
+export default App;
